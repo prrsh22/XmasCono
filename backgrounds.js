c6e6d26d8424ccbd5e8fc21ceed9a6bcc7d1252e
Sohyun <prrsh22@gmail.com>
2020-12-07 19:36:32 +0900

    added final bgs

--- backgrounds.js
+++ backgrounds.js
@@ -1,26 +1,21 @@
 function outsideBG () {
   push();
+  image(bg,450,450,920,920);
+  image(garland,450,85,900,150);
+  image(presents,450,790,700,150);
+
   fill(255);
-  rectMode(CENTER);
+  fill(0);
+  fill(255);
   rect(450,450,750,500);
 
   fill(0);
-  rect(450,800,350,130);
+  rect(450,800,360,150);
   fill(165,203,203);
-  rect(450,800,120,100);
-  fill(242,242,240);
-  rect(335,800,102,100);
-  line(318,750,318,850);
-  line(352,750,352,850);
-  line(284,775,386,775);
-  line(284,800,386,800);
-  line(284,825,386,825);
-  rect(565,800,102,100);
-  line(548,750,548,850);
-  line(582,750,582,850);
-  line(514,775,616,775);
-  line(514,800,616,800);
-  line(514,825,616,825);
+  rect(450,800,330,130);
+
+  image(keyExplantion,450,800,320,110);
+
   pop();
 }
 
@@ -155,154 +150,133 @@ function singBG () {
   ellipse(770,485,40,40); 
   ellipse(800,485,40,40);
   triangle(750,490,820,490,785,540); 
-  textSize(12);
-  text("Merry", 720,555);
-  fill(107,153,0);
-  text("christmas!",770,555); //heart
   pop();
 }
 
 function initialBG() {
   push();
   noStroke();
-  rectMode(CENTER);
-fill(12, 12, 56);
-rect(450, 450, 750, 500);
+  fill(12, 12, 56);
+  rect(450, 450, 750, 500);
 
-//건물2
-fill(143, 166, 162);
-rect(230, 620, 310, 60);
-rect(520, 600, 290, 70);
-rect(700, 620, 250, 70);
-quad(75, 590, 100, 550, 250, 550, 275, 590);
-triangle(530, 570, 600, 530, 680, 570);
-quad(825, 590, 800, 550, 750, 550, 725, 590);
+  //건물2
+  fill(143, 166, 162);
+  rect(230, 620, 310, 60);
+  rect(520, 600, 290, 70);
+  rect(700, 620, 250, 70);
+  quad(75, 590, 100, 550, 250, 550, 275, 590);
+  triangle(530, 570, 600, 530, 680, 570);
+  quad(825, 590, 800, 550, 750, 550, 725, 590);
 
-//건물1
-fill(193, 193, 199);
-rect(215, 650, 280, 60);
-rect(390, 650, 80, 40);
-rect(500, 650, 140, 100);
-rect(570, 630, 100, 140);
-rect(690, 650, 270, 70);
-triangle(215, 630, 145, 580, 75, 630);
-triangle(540, 610, 480, 550, 415, 610);
-triangle(825, 620, 750, 570, 680, 620);
-
-
-//눈밭
-ellipseMode(CENTER)
-fill(256);
-arc(260, 700, 370, 100, PI, 0, CHORD);
-arc(560, 700, 370, 100, PI, 0, CHORD);
-triangle(825, 650, 825, 700, 680, 700);
-triangle(150, 700, 75, 650, 75, 700);
-
-//창문
-fill(252, 244, 3);
-win(120, 630);
-win(230, 580);
-win(400, 600);
-win(500, 610);
-win(770, 630);
-fill(255, 213, 3);
-win(335, 635);
-win(600, 600);
-win(790, 580);
+  //건물1
+  fill(193, 193, 199);
+  rect(215, 650, 280, 60);
+  rect(390, 650, 80, 40);
+  rect(500, 650, 140, 100);
+  rect(570, 630, 100, 140);
+  rect(690, 650, 270, 70);
+  triangle(215, 630, 145, 580, 75, 630);
+  triangle(540, 610, 480, 565, 415, 610);
+  triangle(825, 620, 750, 570, 680, 620);
 
 
-//기둥
-fill(135, 94, 50);
-tree(180, 670);
-tree(300, 650);
-tree(400, 660);
-tree(550, 640);
-tree(670, 650);
-tree(740, 670);
+  //눈밭
+  ellipseMode(CENTER)
+  fill(256);
+  arc(260, 700, 370, 100, PI, 0, CHORD);
+  arc(560, 700, 370, 100, PI, 0, CHORD);
+  triangle(825, 650, 825, 700, 680, 700);
+  triangle(150, 700, 75, 650, 75, 700);
 
-//나무
-christmasTree(180, 670);
-christmasTree(400, 660);
-christmasTree(670, 650);
-christmasTree(740, 670);
-fill(38, 115, 21);
-triangle(300, 570, 330, 640, 270, 640);
-triangle(550, 570, 570, 635, 530, 635);
+  //창문
+  fill(252, 244, 3);
+  win(120, 630);
+  win(230, 580);
+  win(400, 600);
+  win(500, 610);
+  win(770, 630);
+  fill(255, 213, 3);
+  win(335, 635);
+  win(600, 600);
+  win(790, 580);
 
-//장식
-fill(255, 0, 0); //빨
-decoration(190, 600);
-decoration(420, 630);
-decoration(750, 600);
-fill(255, 157, 0); //주
-decoration(195, 640);
-decoration(300, 605);
-decoration(660, 580);
-fill(0, 255, 238); //파
-decoration(170, 620);
-decoration(310, 625);
-decoration(680, 610);
-decoration(755, 643);
-fill(242, 255, 0); //노
-decoration(390, 590);
-decoration(552, 620);
-decoration(655, 630);
-fill(255, 0, 234); //분
-decoration(390, 620);
-decoration(730, 630);
 
-//달
-fill(230, 187, 0);
-ellipse(450, 310, 120, 120);
+  //기둥
+  fill(135, 94, 50);
+  tree(180, 670);
+  tree(300, 650);
+  tree(400, 660);
+  tree(550, 640);
+  tree(670, 650);
+  tree(740, 670);
 
-//종
-fill(77, 255, 216);
-rect(600, 395, 10, 15);
-ellipse(600, 457, 25, 20);
-fill(255, 199, 254);
-arc(600, 450, 60, 100, PI, 0, CHORD);
-ellipse(600, 450, 70, 15);
+  //나무
+  christmasTree(180, 670);
+  christmasTree(400, 660);
+  christmasTree(670, 650);
+  christmasTree(740, 670);
+  fill(38, 115, 21);
+  triangle(300, 570, 330, 640, 270, 640);
+  triangle(550, 570, 570, 635, 530, 635);
 
-if (mouseX < 610 && mouseX > 550 && mouseY < 450 && mouseY > 400) {
-  fill(255, 46, 70);
-  arc(600, 450, 60, 100, PI, 0, CHORD);
-  ellipse(600, 450, 70, 15);
+  //장식
+  fill(255, 0, 0); //빨
+  decoration(190, 600);
+  decoration(420, 630);
+  decoration(750, 600);
+  fill(255, 157, 0); //주
+  decoration(195, 640);
+  decoration(300, 605);
+  decoration(660, 580);
+  fill(0, 255, 238); //파
+  decoration(170, 620);
+  decoration(310, 625);
+  decoration(680, 610);
+  decoration(755, 643);
+  fill(242, 255, 0); //노
+  decoration(390, 590);
+  decoration(552, 620);
+  decoration(655, 630);
+  fill(255, 0, 234); //분
+  decoration(390, 620);
+  decoration(730, 630);
+
+  //달
+  fill(247, 218, 0);
+  ellipse(450, 300, 120, 120);
+
+  //글씨
+  textFont(yun);
+  textSize(80);
+  textAlign(CENTER);
+  fill(256);
+  text("CHRISTMAS CONO", 450, 315);
+
+  textSize(30);
+  fill(256);
+  text("올해도 어김없이 솔크를 맞이하는 당신에게", 370, 390);
+  text("어느날, 익명의 편지가 도착했다!", 560, 415);
+  textSize(40);
+  text("Click a Letter", 450, 532);
+
+  //편지
+  fill(255, 0, 0);
+  rect(450, 480, 90, 48);
+  fill(256);
+  triangle(405, 455, 495, 455, 450, 490);
+  fill(255, 0, 0);
+  triangle(410, 455, 490, 455, 450, 485);
+
+  if (mouseX > 405 && mouseX < 495 && mouseY > 456 && mouseY < 504) {
+    fill(255, 0, 0);
+
+    rect(450, 480, 95, 52);
+    triangle(402, 455, 498, 455, 450, 430);
+
+    fill(250, 242, 215);
+    triangle(402, 455, 498, 455, 450, 487);
+  pop();
 }
-
-//양말
-fill(43, 255, 237);
-ellipse(300, 450, 55, 35);
-rect(313, 420, 30, 60);
-
-fill(255, 69, 69);
-rect(313, 390, 30, 15);
-
-if (mouseX < 325 && mouseX > 275 && mouseY < 437 && mouseY > 395) {
-  fill(0, 120, 247);
-  rect(313, 420, 30, 45);
-  ellipse(300, 450, 55, 35);
- 
-}
-
-//글씨
-textFont(letsPlay);
-textSize(50);
-textAlign(CENTER);
-fill(256);
-text("CHRISTMAS CONO", 450, 325);
-
-textFont(letsPlay);
-textSize(35);
-textAlign(CENTER);
-fill(256);
-text("Let's Play !", 600, 510);
-
-textFont(letsPlay);
-textSize(35);
-textAlign(CENTER);
-fill(256);
-text("How to Play ?", 310, 510);
-pop();
 }
 
 function win(x, y) {
@@ -335,7 +309,9 @@ return d;
 
 function instructionBG() {
   push();
-noStroke();
+  noStroke();
+  if (iStage === 0) {
+    rectMode(CENTER);
   fill(12, 12, 56);
   rect(450, 450, 750, 500);
 
@@ -356,7 +332,7 @@ noStroke();
   rect(570, 630, 100, 140);
   rect(690, 650, 270, 70);
   triangle(215, 630, 145, 580, 75, 630);
-  triangle(540, 610, 480, 550, 415, 610);
+  triangle(540, 610, 480, 565, 415, 610);
   triangle(825, 620, 750, 570, 680, 620);
 
 
@@ -428,8 +404,6 @@ noStroke();
   rect(450, 370, 640, 230);
 
   //양말
-  push();
-  noStroke();
   fill(255, 69, 69);
   ellipse(125, 295, 50, 35);
   rect(135, 270, 30, 50);
@@ -439,7 +413,6 @@ noStroke();
   ellipse(115, 295, 8, 8);
   ellipse(135, 270, 8, 8);
   ellipse(140, 290, 8, 8);
-  pop();
 
   //선물상자
   fill(0, 255, 238);
@@ -458,26 +431,178 @@ noStroke();
   triangle(760, 450, 780, 450, 775, 432);
   triangle(760, 450, 740, 450, 745, 432);
   }
+  
+  
 
   textFont(yun);
   textAlign(CENTER);
-  textSize(28);
+  textSize(40);
+  fill(242, 0, 255);
+  text("<2020 OH! CHRISTMAS PARTY>", 450, 290);
 
-  fill(10, 34, 66);
-  text("2020년에도 어김없이 솔크를 맞이한 당신에게", 450, 290);
-  text("갑작스러운 크리스마스 파티 초대권이 주어졌다", 450, 320);
-  text("익명의 그를 만나러 가기 위해 옷을 고르려면", 450, 350);
-  text("캐롤을 있는 힘껏 열창해야 하는데...", 450, 380);
+  textSize(30);
+  fill(0);
+  text("축하합니다, 당신은 꿈과 사랑이 이루어지는", 450, 330);
+  text("지상 최대 초호화 럭셔리 파티에 초대되셨습니다", 450, 360);
+  text("단, 이곳에 들어오기 위한 한가지 조건이 있습니다", 450, 390);
 
-  textSize(33);
-  fill(230, 25, 52);
-  text("캐롤을 열창하면 볼륨에 따라 옷과 점수가 달라진다", 450, 420);
-  text("파티에 무사히 갈 수 있길, 행운을 빈다.", 450, 450);
+  fill(0, 64, 255);
+  text("1. 파티에 참석하려면 반드시 캐롤을 열창해야 합니다.", 450, 425);
+  text("2. 노래 점수에 따라 파티 의상이 정해집니다.", 450, 455);
   
   textSize(40);
   fill(256);
-  text("START", 760,530);
-pop(); 
+  text("Click!", 760,530);
+  } else {
+    fill(12, 12, 56);
+  rect(450, 450, 750, 500);
+
+  //건물2
+  fill(143, 166, 162);
+  rect(230, 620, 310, 60);
+  rect(520, 600, 290, 70);
+  rect(700, 620, 250, 70);
+  quad(75, 590, 100, 550, 250, 550, 275, 590);
+  triangle(530, 570, 600, 530, 680, 570);
+  quad(825, 590, 800, 550, 750, 550, 725, 590);
+
+  //건물1
+  fill(193, 193, 199);
+  rect(215, 650, 280, 60);
+  rect(390, 650, 80, 40);
+  rect(500, 650, 140, 100);
+  rect(570, 630, 100, 140);
+  rect(690, 650, 270, 70);
+  triangle(215, 630, 145, 580, 75, 630);
+  triangle(540, 610, 480, 565, 415, 610);
+  triangle(825, 620, 750, 570, 680, 620);
+
+
+  //눈밭
+  ellipseMode(CENTER)
+  fill(256);
+  arc(260, 700, 370, 100, PI, 0, CHORD);
+  arc(560, 700, 370, 100, PI, 0, CHORD);
+  triangle(825, 650, 825, 700, 680, 700);
+  triangle(150, 700, 75, 650, 75, 700);
+
+  //창문
+  fill(252, 244, 3);
+  win(120, 630);
+  win(230, 580);
+  win(400, 600);
+  win(500, 610);
+  win(770, 630);
+  fill(255, 213, 3);
+  win(335, 635);
+  win(600, 600);
+  win(790, 580);
+
+
+  //기둥
+  fill(135, 94, 50);
+  tree(180, 670);
+  tree(300, 650);
+  tree(400, 660);
+  tree(550, 640);
+  tree(670, 650);
+  tree(740, 670);
+
+  //나무
+  christmasTree(180, 670);
+  christmasTree(400, 660);
+  christmasTree(670, 650);
+  christmasTree(740, 670);
+  fill(38, 115, 21);
+  triangle(300, 570, 330, 640, 270, 640);
+  triangle(550, 570, 570, 635, 530, 635);
+
+  //장식
+  fill(255, 0, 0); //빨
+  decoration(190, 600);
+  decoration(420, 630);
+  decoration(750, 600);
+  fill(255, 157, 0); //주
+  decoration(195, 640);
+  decoration(300, 605);
+  decoration(660, 580);
+  fill(0, 255, 238); //파
+  decoration(170, 620);
+  decoration(310, 625);
+  decoration(680, 610);
+  decoration(755, 643);
+  fill(242, 255, 0); //노
+  decoration(390, 590);
+  decoration(552, 620);
+  decoration(655, 630);
+  fill(255, 0, 234); //분
+  decoration(390, 620);
+  decoration(730, 630);
+
+  //설명
+  fill(242, 248, 252);
+  rect(450, 370, 650, 240);
+  fill(214, 214, 214);
+  rect(450, 370, 640, 230);
+
+  //양말
+  fill(255, 69, 69);
+  ellipse(125, 295, 50, 35);
+  rect(135, 270, 30, 50);
+  fill(0, 255, 30);
+  rect(135, 250, 30, 15);
+  fill(235, 251, 41);
+  ellipse(115, 295, 8, 8);
+  ellipse(135, 270, 8, 8);
+  ellipse(140, 290, 8, 8);
+
+  //선물상자
+  fill(0, 255, 238);
+  quad(730, 450, 790, 450, 790, 505, 730, 505);
+  fill(255, 0, 0);
+  rect(760, 477, 8, 57);
+  rect(760, 475.5, 60, 8);
+  triangle(760, 450, 780, 450, 775, 432);
+  triangle(760, 450, 740, 450, 745, 432);
+
+  if (mouseX > 730 && mouseX < 790 && mouseY > 450 && mouseY < 505) {
+    fill(255, 251, 0);
+    quad(730, 450, 790, 450, 790, 505, 730, 505);
+    fill(255, 0, 0);
+    rect(760, 477, 8, 57);
+    rect(760, 475.5, 60, 8);
+    triangle(760, 450, 780, 450, 775, 432);
+    triangle(760, 450, 740, 450, 745, 432);
+  }
+
+
+
+  textFont(yun);
+  textAlign(CENTER);
+  textSize(40);
+  fill(242, 0, 255);
+  text("<2020 OH! CHRISTMAS PARTY>", 450, 290);
+
+  textSize(30);
+  fill(0);
+  text("1. 원하는 곡목을 골라 노래를 불러주세요", 450, 340);
+  text("2. 파트별 마이크 볼륨에 따라 옷이 달라집니다", 450, 370);
+  text("3. 만약 볼륨이 너무 낮아질시, 당신의 얼굴이 캐릭터를 대체합니다", 450, 400);
+
+  fill(0, 64, 255);
+  text("* 하단에 있는 노래방 단축키를 참고하세요 *", 450, 445);
+  
+  fill(255, 106, 0);
+  text("마이크 볼륨", 392, 370);
+  text("옷", 515, 370);
+  text("당신의 얼굴", 475, 400);
+
+
+  textSize(40);
+  fill(256);
+  text("Start!", 760, 530);
+  }
+  pop();
 }
 
 function endingBG() {
@@ -561,6 +686,7 @@ textSize(35);
   pop();
 }
 
+
 function snowball(x, y) {
 let s =
   //280,230    
@@ -605,144 +731,196 @@ function readyBG() {
 push();
 noStroke();
 fill(12, 12, 56);
-rect(450, 450, 750, 500);
+  rect(450, 450, 750, 500);
 
-//건물2
-fill(143, 166, 162);
-rect(230, 620, 310, 60);
-rect(520, 600, 290, 70);
-rect(700, 620, 250, 70);
-quad(75, 590, 100, 550, 250, 550, 275, 590);
-triangle(530, 570, 600, 530, 680, 570);
-quad(825, 590, 800, 550, 750, 550, 725, 590);
+  //건물2
+  fill(143, 166, 162);
+  rect(230, 620, 310, 60);
+  rect(520, 600, 290, 70);
+  rect(700, 620, 250, 70);
+  quad(75, 590, 100, 550, 250, 550, 275, 590);
+  triangle(530, 590, 600, 550, 680, 590);
+  quad(825, 590, 800, 550, 750, 550, 725, 590);
 
-//건물1
-fill(193, 193, 199);
-rect(215, 650, 280, 60);
-rect(390, 650, 80, 40);
-rect(500, 650, 140, 100);
-rect(570, 630, 100, 140);
-rect(690, 650, 270, 70);
-triangle(215, 630, 145, 580, 75, 630);
-triangle(540, 610, 480, 550, 415, 610);
-triangle(825, 620, 750, 570, 680, 620);
-
-
-//눈밭
-ellipseMode(CENTER)
-fill(256);
-arc(260, 700, 370, 100, PI, 0, CHORD);
-arc(560, 700, 370, 100, PI, 0, CHORD);
-triangle(825, 650, 825, 700, 680, 700);
-triangle(150, 700, 75, 650, 75, 700);
-
-//창문
-fill(252, 244, 3);
-win(120, 630);
-win(230, 580);
-win(400, 600);
-win(500, 610);
-win(770, 630);
-fill(255, 213, 3);
-win(335, 635);
-win(600, 600);
-win(790, 580);
+  //건물1
+  fill(193, 193, 199);
+  rect(215, 650, 280, 60);
+  rect(390, 650, 80, 40);
+  rect(500, 650, 140, 100);
+  rect(570, 630, 100, 140);
+  rect(690, 650, 270, 70);
+  triangle(215, 630, 145, 580, 75, 630);
+  triangle(540, 610, 480, 550, 415, 610);
+  triangle(825, 620, 750, 570, 680, 620);
 
 
-//기둥
-fill(135, 94, 50);
-tree(180, 670);
-tree(300, 650);
-tree(400, 660);
-tree(550, 640);
-tree(670, 650);
-tree(740, 670);
+  //눈밭
+  ellipseMode(CENTER)
+  fill(256);
+  arc(260, 700, 370, 100, PI, 0, CHORD);
+  arc(560, 700, 370, 100, PI, 0, CHORD);
+  triangle(825, 650, 825, 700, 680, 700);
+  triangle(150, 700, 75, 650, 75, 700);
 
-//나무
-christmasTree(180, 670);
-christmasTree(400, 660);
-christmasTree(670, 650);
-christmasTree(740, 670);
-fill(38, 115, 21);
-triangle(300, 570, 330, 640, 270, 640);
-triangle(550, 570, 570, 635, 530, 635);
-
-//장식
-fill(255, 0, 0); //빨
-decoration(190, 600);
-decoration(420, 630);
-decoration(750, 600);
-fill(255, 157, 0); //주
-decoration(195, 640);
-decoration(300, 605);
-decoration(660, 580);
-fill(0, 255, 238); //파
-decoration(170, 620);
-decoration(310, 625);
-decoration(680, 610);
-decoration(755, 643);
-fill(242, 255, 0); //노
-decoration(390, 590);
-decoration(552, 620);
-decoration(655, 630);
-fill(255, 0, 234); //분
-decoration(390, 620);
-decoration(730, 630);
-
-//화면
-fill(256);
-rect(450, 370, 650, 240);
-fill(214, 214, 214);
-rect(450, 370, 640, 230);
-fill(256);
-rect(450, 300, 650, 5);
-for (i = 300; i < 490; i += 38) {
-  rect(450, i, 650, 3);
-}
-rect(270, 370, 3, 240);
-rect(540, 370, 3, 240);
+  //창문
+  fill(252, 244, 3);
+  win(120, 630);
+  win(230, 580);
+  win(400, 600);
+  win(500, 610);
+  win(770, 630);
+  fill(255, 213, 3);
+  win(335, 635);
+  win(600, 600);
+  win(790, 580);
 
 
-textFont(yun);
-textSize(35);
-textAlign(CENTER, BASELINE);
-fill(10, 34, 66);
-text("NUMBER", 205, 285);
-text("SONG", 400,285);
-text("ARTIST", 655,285);
+  //기둥
+  fill(135, 94, 50);
+  tree(180, 670);
+  tree(300, 650);
+  tree(400, 660);
+  tree(550, 640);
+  tree(670, 650);
+  tree(740, 670);
+
+  //나무
+  christmasTree(180, 670);
+  christmasTree(400, 660);
+  christmasTree(670, 650);
+  christmasTree(740, 670);
+  fill(38, 115, 21);
+  triangle(300, 570, 330, 640, 270, 640);
+  triangle(550, 570, 570, 635, 530, 635);
+
+  //장식
+  fill(255, 0, 0); //빨
+  decoration(190, 600);
+  decoration(420, 630);
+  decoration(750, 600);
+  fill(255, 157, 0); //주
+  decoration(195, 640);
+  decoration(300, 605);
+  decoration(660, 580);
+  fill(0, 255, 238); //파
+  decoration(170, 620);
+  decoration(310, 625);
+  decoration(680, 610);
+  decoration(755, 643);
+  fill(242, 255, 0); //노
+  decoration(390, 590);
+  decoration(552, 620);
+  decoration(655, 630);
+  fill(255, 0, 234); //분
+  decoration(390, 620);
+  decoration(730, 630);
+
+  //화면
+  fill(256);
+  rect(450, 370, 650, 240);
+  fill(214, 214, 214);
+  rect(450, 370, 640, 230);
+  fill(256);
+  rect(450, 300, 650, 5);
+  for (i = 300; i < 490; i += 38) {
+    rect(450, i, 650, 3);
+  }
+  rect(270, 370, 3, 240);
+  rect(540, 370, 3, 240);
 
 
-textSize(30);
-text("12250", 205, 327);
-text("Must Have Love", 400, 327);
-text("SG워너비 & 브아걸", 655, 327)
-text("미리 메리크리스마스", 400, 367);
-text("아이유 feat.천둥", 655, 367);
-text("겨울 고백", 400, 405);
-text("성시경, 박효신, 이석훈 등", 655, 405);
 
-//양말
-push();
-noStroke();
-fill(255, 69, 69);
-ellipse(125, 295, 50, 35);
-rect(135, 270, 30, 50);
-fill(0, 255, 30);
-rect(135, 250, 30, 15);
-fill(235, 251, 41);
-ellipse(115, 295, 8, 8);
-ellipse(135, 270, 8, 8);
-ellipse(140, 290, 8, 8);
-pop();
 
-//선물상자
-fill(0, 255, 238);
-quad(730, 450, 790, 450, 790, 505, 730, 505);
-fill(255, 0, 0);
-rect(760, 477, 8, 57);
-rect(760, 475.5, 60, 8);
-triangle(760, 450, 780, 450, 775, 432);
-triangle(760, 450, 740, 450, 745, 432);
+
+  textFont(yun);
+  textSize(35);
+  textAlign(CENTER, BASELINE);
+  fill(10, 34, 66);
+  text("NUMBER", 205, 285);
+  text("SONG", 400, 285);
+  text("ARTIST", 655, 285);
+
+  let songList = [
+    [['12250', 'Must Have Love', 'SG워너비 & 브아걸'],
+    ['10290', 'All I Want for Christmas Is You', 'Mariah Carey'],
+    ['12170', '우리 사랑하게 됐어요', '조권 & 가인'],
+    ['93626', 'Santa Tell Me', 'Ariana Grande'],
+    ['93516', '미리메리크리스마스', '아이유 (Feat. 천둥)']],
+
+    [['12408', '첫눈', 'EXO'], ['','',''], ['','',''], ['','',''], ['','','']]
+  ];
+
+
+    textSize(30);
+
+    text(songList[page][0][1], 400, 325);
+    text(songList[page][0][2], 655, 325);
+    if (!page) {
+      push();
+      textSize(25);
+      text(songList[page][1][1], 400, 367);
+      pop();
+    } else {
+      text(songList[page][1][1], 400, 367);
+    }
+    text(songList[page][1][2], 655, 367);
+    text(songList[page][2][1], 400, 405);
+    text(songList[page][2][2], 655, 405);
+    text(songList[page][3][1], 400, 440);
+    text(songList[page][3][2], 655, 440);
+    text(songList[page][4][1], 400, 475);
+    text(songList[page][4][2], 655, 475);
+
+    textSize(35);
+    text(songList[page][0][0], 200, 325);
+    text(songList[page][1][0], 200, 365);
+    text(songList[page][2][0], 200, 402);
+    text(songList[page][3][0], 200, 438);
+    text(songList[page][4][0], 200, 473);
+  
+
+  //다음
+  fill(256);
+  text("PRESS THE NUMBER > PRESS ENTER", 450,237);
+
+  if (!page){
+  triangle(695, 520, 695, 500, 710, 510);
+  textSize(35)
+  text(`PAGE 1/2`, 630, 515);
+  if (mouseX > 565 && mouseX < 710 && mouseY > 500 && mouseY < 520) {
+    fill(242, 255, 0);
+    triangle(695, 520, 695, 500, 710, 510);
+    text(`PAGE 1/2`, 630, 515);
+  }} else {
+    triangle(580, 520, 580, 500, 565, 510);
+    textSize(35);
+    text("PAGE 2/2", 655, 515);
+    if (mouseX > 565 && mouseX < 710 && mouseY > 500 && mouseY < 520) {
+      fill(242, 255, 0);
+      triangle(580, 520, 580, 500, 565, 510);
+      text("PAGE 2/2", 655, 515);
+    }
+  }
+
+  //양말
+  fill(255, 69, 69);
+  ellipse(125, 295, 50, 35);
+  rect(135, 270, 30, 50);
+  fill(0, 255, 30);
+  rect(135, 250, 30, 15);
+  fill(235, 251, 41);
+  ellipse(115, 295, 8, 8);
+  ellipse(135, 270, 8, 8);
+  ellipse(140, 290, 8, 8);
+
+  //선물상자
+  fill(0, 255, 238);
+  quad(730, 450, 790, 450, 790, 505, 730, 505);
+  fill(255, 0, 0);
+  rect(760, 477, 8, 57);
+  rect(760, 475.5, 60, 8);
+  triangle(760, 450, 780, 450, 775, 432);
+  triangle(760, 450, 740, 450, 745, 432);
 
 pop();
 }
