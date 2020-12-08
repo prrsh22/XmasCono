cea74dc2631e95687750ac49fc56aeb247f04549
Sohyun <prrsh22@gmail.com>
2020-12-08 12:10:49 +0900

    added songs

--- constants.js
+++ constants.js
@@ -1,5 +1,3 @@
-const instructions = [];
-
 const songs = {
     '12250': {
         title: 'Must Have Love',
@@ -31,55 +29,54 @@ const songs = {
             [184.8, '그대와 함께'], [187, '나누고 싶어'], [191, '사랑인가봐 너와 나','face'], 
             [195, '시작됐나봐 너와 나'], [199, '우리 둘 사랑하게 됐다고'], [203, '말할 수 있어'], [209, ' ']],
         
-            v1 : [[19.5 , '전주 중'], [24, '맨 처음 그날의 난'], [28, '아마 몰랐겠지 이런 날','top'], 
-            [32.5, '어색했었던 우리 모습이'], [36.8, '어제만 같은데',], [40.8, '기억하고 있을까'], 
-            [44.8, '우리 잠시 멀리 있던 날','bottom'], [48.8, '잠들기 힘든 내 모습'], [54.5, '내 맘이 자꾸 널 찾던 날~'], 
-            [59, '어떤 맘일까 그대와 나'], [63, '같은 맘일까 그대와 나','shoes'], [67, '한가지 내게 분명한 것은'],
+            v1 : [[2, ' '], [19.5 , '전주 중'], [24, '맨 처음 그날의 난'], [28, '아마 몰랐겠지 이런 날'], 
+            [32.5, '어색했었던 우리 모습이','top'], [36.8, '어제만 같은데',], [40.8, '기억하고 있을까'], 
+            [44.8, '우리 잠시 멀리 있던 날'], [48.8, '잠들기 힘든 내 모습','bottom'], [54.5, '내 맘이 자꾸 널 찾던 날~'], 
+            [59, '어떤 맘일까 그대와 나'], [63, '같은 맘일까 그대와 나'], [67, '한가지 내게 분명한 것은','shoes'],
             [71, '함께 있어야 웃는다는 것'],[75, '사랑인걸까 너와 나'], [79.8, '같은 맘일까 너와 나','face'], 
             [83, '우리가 사랑하게 됐다고'], [86, '말해도 될까'], [89, ' ']]
             }
     },
 
     '93626': {
-        title : 'Santa tell me',
+        title : 'Santa Tell Me',
           singer : 'Ariana Grande',
            lyrics : {
-                //https://www.youtube.com/watch?v=ByqHu6UqDmk
-                hl : [[160 , ' '], [164.8, 'Santa, tell me if you are really there'], [169, 'Dont make me fall in love again if he wont be here'],
-                [170.8, 'Next year','top'], [175, 'Santa, tell me if he really cares'], [179, 'Cause I cant give it all away if he wont be here'],
-                [181, 'Next year','bottom'], [185, 'Santa, tell me if you are really there'], [189, 'Dont make me fall in love again if he wont be here','shoes'],
-                [191.8, 'Next year'], [194.8, 'Santa, tell me if he really cares','face'], [199, 'Cause I cant give it all away if he wont be here'],
+                hl : [[160 , ' '], [164.8, 'Santa, tell me if you are really there'], [169, 'Don\'t make me fall in love again if he won\'t be here'],
+                [170.8, 'Next year','top'], [175, 'Santa, tell me if he really cares'], [179, 'Cause I can\'t give it all away if he won\'t be here'],
+                [181, 'Next year','bottom'], [185, 'Santa, tell me if you are really there'], [189, 'Dont make me fall in love again if he won\'t be here','shoes'],
+                [191.8, 'Next year'], [194.8, 'Santa, tell me if he really cares','face'], [199, 'Cause I cant give it all away if he won\'t be here'],
                 [202, 'Next year'],  [204, ' ']],
                 
-                v1 : [[9.7, '전주 중'], [14.8, 'Santa, tell me if you are really there'], [19, 'Dont make me fall in love again if he wont be here'], 
-                [20.8, 'Next year','top'], [25, 'Santa, tell me if he really cares'], [29.5, 'Cause I cant give it all away if he wont be here'], 
-                [31, 'Next year','bottom'], [33.8, 'Feeling Christmas all around'], [36, 'And Im trying play it cool'],
-                [37.8, 'But its hard to focus'], [41, 'When I see him walking cross the room','shoes'], [43.5, 'Let it snow is blasting out'], 
-                [46, 'But I wont get in the mood'], [50 , 'Im avoiding every mistletoe until I know','face'], 
-                [54, 'Its true love that he thinks of'], [56.8, 'So next Christmas'], [60, 'Im not all alone, boy'], [60.5, ' ']] 
+                v1 : [[9.7, '전주 중'], [14.8, 'Santa, tell me if you are really there'], [19, 'Don\'t make me fall in love again if he won\'t be here'], 
+                [20.8, 'Next year','top'], [25, 'Santa, tell me if he really cares'], [29.5, 'Cause I can\'t give it all away if he won\'t be here'], 
+                [31, 'Next year','bottom'], [33.8, 'Feeling Christmas all around'], [36, 'And I\'m trying play it cool'],
+                [37.8, 'But it\'s hard to focus'], [41, 'When I see him walking cross the room','shoes'], [43.5, 'Let it snow is blasting out'], 
+                [46, 'But I won\'t get in the mood'], [50 , 'I\'m avoiding every mistletoe until I know','face'], 
+                [54, 'It\'s true love that he thinks of'], [56.8, 'So next Christmas'], [60, 'I\'m not all alone, boy'], [60.5, ' ']] 
             }
     },
 
-    '202012' : {
-        tittle : 'All I want for Chirstmas is you',
+    '10290' : {
+        title : 'All I Want for Chirstmas Is You',
           singer : 'Mariah Carey',
             lyrics : {
               
-                hl : [[57 , ' '], [60, 'I dont want a lot for Christmas'], [63, 'There is just one thing I need'], 
-                [66, 'I dont care about the presents','top'], [70, 'Underneath the Christmas tree'], 
-                [73, 'I dont need to hang my stocking'], [76, 'There upon the fireplace','bottom'], 
-                [79.8, 'Santa Claus wont make me happy'], [82.8, 'With a toy on Christmas day'],
+                hl : [[57 , ' '], [60, 'I don\'t want a lot for Christmas'], [63, 'There is just one thing I need'], 
+                [66, 'I don\'t care about the presents','top'], [70, 'Underneath the Christmas tree'], 
+                [73, 'I don\'t need to hang my stocking'], [76, 'There upon the fireplace','bottom'], 
+                [79.8, 'Santa Claus won\'t make me happy'], [82.8, 'With a toy on Christmas day'],
                 [85.8, 'I just want you for my own','shoes'], [89, 'More than you could ever know'], 
                 [92, 'Make my wish come true','face'], [98, 'All I want for Christmas is you'], [101,'You baby'],
                 [102, ' ']],
                     
-                v1 : [[9 , '전주 중'], [15, 'I dont want a lot for Christmas'], [19.8, 'There is just one thing I need','top'],
-                [23.8, 'I dont care about the presents'], [26.8, 'Underneath the Christmas tree'],
+                v1 : [[3, ' '], [9 , '전주 중'], [15, 'I don\'t want a lot for Christmas'], [19.8, 'There is just one thing I need','top'],
+                [23.8, 'I don\'t care about the presents'], [26.8, 'Underneath the Christmas tree'],
                 [29.8, 'I just want you for my own'], [33.8, 'More than you could ever know'],
                 [39, 'Make my wish come true'], [57, 'All I want for Christmas is you','bottom'],
-                [60, 'I dont want a lot for Christmas'], [63, 'There is just one thing I need'],
-                [66, 'I dont care about the presents'], [70, 'Underneath the Christmas tree','shoes'], 
-                [73, 'I dont need to hang my stocking'], [76, 'There upon the fireplace'], [79.8, 'Santa Claus wont make me happy'], 
+                [60, 'I don\'t want a lot for Christmas'], [63, 'There is just one thing I need'],
+                [66, 'I don\'t care about the presents'], [70, 'Underneath the Christmas tree','shoes'], 
+                [73, 'I don\'t need to hang my stocking'], [76, 'There upon the fireplace'], [79.8, 'Santa Claus wont make me happy'], 
                 [82.8, 'With a toy on Christmas day'],[85.8, 'I just want you for my own','face'], 
                 [89, 'More than you could ever know'], [92, 'Make my wish come true'], [98, 'All I want for Christmas is you'], 
                 [101,'You baby'] ,[102, ' ']]
@@ -91,20 +88,20 @@ const songs = {
           singer : '아이유(feat.천둥)',
             lyrics : {
               
-                hl : [[89 , ' '], [92.8, '하얀 눈이 내려올때면'], [97, '온 세상이 물들을때면','top'], 
-                [99, '눈꽃이 피어나'], [100.8, '또 빛이나'], [103.8, '눈이 부신 너처럼','bottom'], 
-                [107.8, '사랑할수 밖에 없어서'], [112, '사랑한 네 손 잡고서~','shoes'], 
-                [115.5, '첫눈 위를 걸어 발자국을 새겨','face'], [117, '이 길 끝까지'], 
+                hl : [[89 , ' '], [92.8, '하얀 눈이 내려올때면'], [97, '온 세상이 물들을때면'], 
+                [99, '눈꽃이 피어나','top'], [100.8, '또 빛이나'], [103.8, '눈이 부신 너처럼'], 
+                [107.8, '사랑할수 밖에 없어서','bottom'], [112, '사랑한 네 손 잡고서~'], 
+                [115.5, '첫눈 위를 걸어 발자국을 새겨','shoes'], [117, '이 길 끝까지','face'], 
                 [121, '걸어갈거야 With you'],  [123, ' ']],
                 
                 v1 : [[44, ' '], [49, '쿵쿵 가슴이 왜 이렇게 가쁘니 yeah'], [51, '꾹꾹 참아도 자꾸'], 
                 [52.8, '네 생각이 나잖아 Oh'], [54, 'You You I love you'], [56, 'and I know you love me too'], 
-                [60, '네가 내게 불러준 you\'re my boo','top'], [63.8, 'You 아직 꿈만 같아'], 
-                [67, 'And you 마치 선물같아'],[70.8, 'For you  네 품에 안겨도','bottom'], 
-                [72.8, '늘 불안 불안해'], [75, '꿈처럼 깰까봐'], [79.5, 'You 꼭 안아줘야만해'], 
-                [82, 'And you 아껴줘야만해'], [85, 'For you 큰맘 먹고'], [89, '네꺼 돼준걸 하늘에게 감사해','shoes'], 
+                [60, '네가 내게 불러준 you\'re my boo'], [63.8, 'You 아직 꿈만 같아','top'], 
+                [67, 'And you 마치 선물같아'],[70.8, 'For you  네 품에 안겨도'], 
+                [72.8, '늘 불안 불안해','bottom'], [75, '꿈처럼 깰까봐'], [79.5, 'You 꼭 안아줘야만해'], 
+                [82, 'And you 아껴줘야만해'], [85, 'For you 큰맘 먹고','shoes'], [89, '네꺼 돼준걸 하늘에게 감사해'], 
                 [92.8, '하얀 눈이 내려올때면'], [97, '온 세상이 물들을때면'], [99, '눈꽃이 피어나'], 
-                [100.8, '또 빛이나',], [103.8, '눈이 부신 너처럼','face'], [107.8, '사랑할수 밖에 없어서'], 
+                [100.8, '또 빛이나',], [103.8, '눈이 부신 너처럼'], [107.8, '사랑할수 밖에 없어서','face'], 
                 [112, '사랑한 네 손 잡고서~'], [115.5, '첫눈 위를 걸어 발자국을 새겨'], [117, '이 길 끝까지'], 
                 [121, '걸어갈거야 With you'],  [123, ' ']]
             }
@@ -112,26 +109,94 @@ const songs = {
 
     '12408' : {
         title : '첫눈',
-        singer : '엑소',
+        singer : 'EXO',
           lyrics : {
             
             hl : [[158 , ' '], [162.7, '너를 만나면 눈물 차 올라'], [165, '바보같은 난 아무 말 못해'], 
-            [168.8, '말해줘 메리 메리 크리스마스'], [171, '안녕 잘지내는거지','top'], [174, '눈이 내리면 멍든 가슴이'],
-            [177, '모두 하얗게 다 덮여지게 될까'], [179.8, '미안해 잘해주지 못해','bottom'], [182.2, '후회만 가득 가득했던'], 
-            [185, '너를 만나면 눈물 차 올라'],[188,'바보같은 난 아무 말 못해','shoes'], [192,'메리 메리 크리스마스'], 
-            [194.1,'잘 지내는거지'], [197,'눈이 내리면 멍든 가슴이'], [200,'모두 하얗게 다 덮여지게 될까','face'], 
-            [202.8,'눈물인지 눈 때문인지'], [206,'점점 너 멀리 멀리 보이던'],[208,'그 크리스마스'], [210,' ']],
+            [168.8, '말해줘 메리 메리 크리스마스'], [171, '안녕 잘지내는거지'], [174, '눈이 내리면 멍든 가슴이','top'],
+            [177, '모두 하얗게 다 덮여지게 될까'], [179.8, '미안해 잘해주지 못해'], [182.2, '후회만 가득 가득했던','bottom'], 
+            [185, '너를 만나면 눈물 차 올라'],[188,'바보같은 난 아무 말 못해'], [192,'메리 메리 크리스마스','shoes'], 
+            [194.1,'잘 지내는거지'], [197,'눈이 내리면 멍든 가슴이'], [200,'모두 하얗게 다 덮여지게 될까'], 
+            [202.8,'눈물인지 눈 때문인지','face'], [206,'점점 너 멀리 멀리 보이던'],[208,'그 크리스마스'], [210,' ']],
             
-            v1 : [[15, '전주 중'], [18, '첫눈 오는 이런 오후에'], [22, '너에게 전화를 걸수만'], [27, '있다면 기쁠텐데','top'], 
-            [30, '벌써 일년이 지났는데'], [33.8, '난 아직 미련 가득해서'], [38, '쓸쓸해 어느새 혼잣말~','bottom'], 
-            [39.2, '시계를 되돌려'], [41, '1년 전으로 갈수 있다면'],[42, '마음을 되돌려'], [44, '지금 우린 달라졌을까'], 
-            [46.8, 'yeah 바보같은 소리지'], [49, '그래도 만약~','shoes'],[52,'너를 만나면 눈물 차올라'], 
-            [55,'바보같은 난 아무 말 못해'], [58.8,'말해줘 메리 메리 크리스마스'], [61,'안녕 잘 지내는거지','face'], 
-            [63.8,'눈이 내리면 멍든 가슴이'], [67,'모두 하얗게 다 덮여지게 될까'], [69.8,'미안해 잘해주지 못해'], 
+            v1 : [[4, ' '], [15, '전주 중'], [18, '첫눈 오는 이런 오후에'], [22, '너에게 전화를 걸수만'], [27, '있다면 기쁠텐데'], 
+            [30, '벌써 일년이 지났는데','top'], [33.8, '난 아직 미련 가득해서'], [38, '쓸쓸해 어느새 혼잣말~'], 
+            [39.2, '시계를 되돌려','bottom'], [41, '1년 전으로 갈수 있다면'],[42, '마음을 되돌려'], [44, '지금 우린 달라졌을까'], 
+            [46.8, 'yeah 바보같은 소리지'], [49, '그래도 만약~'],[52,'너를 만나면 눈물 차올라','shoes'], 
+            [55,'바보같은 난 아무 말 못해'], [58.8,'말해줘 메리 메리 크리스마스'], [61,'안녕 잘 지내는거지'], 
+            [63.8,'눈이 내리면 멍든 가슴이','face'], [67,'모두 하얗게 다 덮여지게 될까'], [69.8,'미안해 잘해주지 못해'], 
             [72.8,'후회만 가득 가득했던'],[75,'그 크리스마스'], [79,' ']]
         }
-    }
+    },
+
+    '14116' : {
+        title : 'Let it go',
+          singer : 'Idina Menzel',
+        lyrics : {
+          
+            hl : [[184 , ' '], [188, 'Let it go Let it go'], [191.8, 'And I will rise like the break of dawn','top'], 
+            [195, 'Let it go Let it go'], [199, 'That perfect girl is gone','bottom'], 
+            [202, 'Here I stand'],[207.7, 'In a light of day','shoes'], [213, 'Let the storm rage on','face'], 
+            [218, 'The cold never bothered me anyway'], [220, ' ']],
+            
+            v1 : [[3.3, ' '], [16, '전주 중'],[20.5 , 'The snow goes white on the mountain tonight'], [23.8, 'Not a footprint to be seen'], 
+            [27, 'A kingdom of isolation'], [28.8, 'and it looks like'], [31, 'I\'m a Queen','top'], 
+            [34.6, 'The wind is howling like this'], [38, 'swirling storm inside'],
+            [40, 'Couldn\'t keep it in'], [45, 'Heaven knows I\'ve tried','bottom'],
+            [47, 'Don\'t let them in'], [49, 'Don\'t let them see'], [52, 'Be the good girl you always have to be'],
+            [54, 'Conceal don\'t feel'], [58, 'don\'t let them know'], 
+            [62, 'Well now they know','shoes'], [65.8, 'Let it go Let it go'],
+            [69, 'Can\'t hold it back anymore'], [72.8, 'Let it go Let it go'], 
+            [76.8, 'Turn away and slam the door','face'], [79.8, 'I don\'t care'],
+            [83, 'what they\'re going to say'], [87, 'Let the storm rage on'], 
+            [90, 'The cold never bothered me anyway'],  [93, ' ']]
+        }
+    },
+
+    '75218': {
+        title : 'Snowman',
+          singer : 'Sia',
+           lyrics : {
+          
+                hl : [[45 , ' '],  [49, 'So come on let\'s go'], [52, 'Let\'s go below zero And hide from the sun','top'], 
+                [55.7, 'I love you forever And we\'ll have some fun'],[59, 'Yes let\'s hit the north pole And live happily','bottom'], 
+                [62.2, 'Please don\'t cry no tears now It\'s christmas baby','shoes'], 
+                [69, 'My snowman and me yea','face'], [75, 'My snowman and me'],[78, 'Baby'], [82, ' ']],
+                
+                v1 : [[1, ' '], [6.5 , '전주 중'], [11, 'Dont cry snowman right in front of me'], 
+                [16, 'Who will catch your tears if you can\'t catch me, darling'], 
+                [20.7, 'If you can\'t catch me, darling','top'], [25, 'Don\'t cry snowman don\'t leave me this way',], 
+                [29.8, 'A puddle of water can\'t hold me close baby'], [35, 'Can\'t hold me close baby','bottom'], 
+                [38.6, 'I want you to know that I\'m never leaving'], [41.8, 'Cause I\'ll miss the snow till death will be freezing'],
+                [45, 'Yea you are my home my home for all seasons'], [49, 'So come on let\'s go','shoes'], 
+                [52, 'Let\'s go below zero And hide from the sun'], [55.7, 'I love you forever And we\'ll have some fun'],
+                [59, 'Yes let\'s hit the north pole And live happily'], [62.2, 'Please don\'t cry no tears now It\'s christmas baby','face'], 
+                [69, 'My snowman and me yea'], [75, 'My snowman and me'],[78, 'Baby'], [82, ' ']]
+            }
+    },
+
+    '60910' : {
+        title : '하얀 고백',
+          singer : '인피니트',
+            lyrics : {
+              
+                hl : [[131, ' '], [138.2, '미안해 못보여준 내 맘을~ 예'], [142.8, '못들려준 대답을~ 예'], 
+                [147.7, '하얀 눈과 함께 찾아가서','top'], [150.5, '말할게~ 예'], [154, '니 눈 보고 말~할게~ 예'], 
+                [158.7, '내게 줬던 사~랑을~ 예','bottom'], [165.7, '이젠 내가 네게 다주고 싶~어'], 
+                [170.6, '아직 잘있~지 아직 잘있~지'],[176.8, '특별하던 너인걸','shoes'], [180, 'woo woo woo woo'], 
+                [182, '메리크리스마스','face'],[183, ' ']],
+                    
+                v1 : [[3.3, ' '], [19.6 , '전주 중'], [23.8, '골목길을 돌아 들어가면'], [28, '혹시라도 그 애가 나타나','top'], 
+                [32.8, '다시 또 놀래킬지 몰라요~'], [35.8, 'I can fly and fly again'], [39.8, '까만 밤에 하얗게 빛나던'], 
+                [44, '흰눈 속에 유난히 빛나던'], [48.8, '어쩌면 난 어쩌면 난~'], [51.6, '아직도~ 널~','bottom'],
+                [58, '사랑해 흩날리는 바람을~ 예'], [63, '못전해진 고백을~ 예'], [67.6, '하얀 눈과 함께 찾아가서'], 
+                [70.3, '말할게~ 예','shoes'], [74, '니 눈 보고 말~할게~ 예'], [78.8, '내게 남은 사~랑을~ 예'], 
+                [86.8, '이제라도 네게 건네고 싶~어'], [89.8, '아직 잘있~지 아직 잘있~지'],[96.8, '특별하던 너인걸','face'], 
+                [99, 'woo woo woo'], [99.5, ' ']]
+                }
+      }
+
   
 }
 
-const comments = [];
+const comments = ['오늘 코디는\n환상이야~', '오늘 코디는\n멋지지만 뭔가 부족해~', '오늘 코디는\n좀 실망인걸~'];
